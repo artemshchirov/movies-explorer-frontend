8de91ebda37ecf36e9598dd8efc289c9e6f300b7
Artem Shchirov <artemshchirov@gmail.com>
2022-09-24 11:57:40 +0300

    add Main header, Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,47 @@
 import './Main.css';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+import Promo from './Promo/Promo';
+import NavTab from './NavTab/NavTab';
+import AboutProject from './AboutProject/AboutProject';
+import Techs from './Techs/Techs';
+import AboutMe from './AboutMe/AboutMe';
+import Portfolio from './Portfolio/Portfolio';
 
 const Main = () => {
   return (
-    <></>
+    <>
+      <header className="header">
+        <Link to={'/'}>
+          <img
+            className="header__logo"
+            src={logo}
+            alt="Логотип 'Movies Explorer'"
+          />
+        </Link>
+
+        <ul className="header__links">
+          <li>
+            <Link className="header__register" to="/signup">
+              Регистрация
+            </Link>
+          </li>
+          <li>
+            <Link className="header__login" to="/signin">
+              <button className="header__login-btn">Войти</button>
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <main className="content page__content">
+        <Promo />
+        <NavTab />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+        <Portfolio />
+      </main>
+    </>
   );
 };
 
