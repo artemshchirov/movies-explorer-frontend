291cc3a5364ebb36b462b08276ac014edc705f09
Artem Shchirov <artemshchirov@gmail.com>
2022-09-25 02:27:51 +0300

    add Portfolio 320px 768px 1280px

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -2,7 +2,41 @@ import './Portfolio.css';
 
 const Portfolio = () => {
   return (
-    <></>
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__links">
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://artemshchirov.github.io/how-to-learn/#en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Статичный сайт
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://artemshchirov.github.io/russian-travel/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Адаптивный сайт
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://artemshchirov.github.io/around/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Одностраничное приложение
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 };
 
