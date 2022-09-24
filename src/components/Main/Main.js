831e2ed1cc6e76ad8f149511ce1ef83caffec69f
Artem Shchirov <artemshchirov@gmail.com>
2022-09-24 13:42:04 +0300

    add NavTab

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import './Main.css';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import Promo from './Promo/Promo';
-import NavTab from './NavTab/NavTab';
+import NavTab from '../NavTab/NavTab';
 import AboutProject from './AboutProject/AboutProject';
 import Techs from './Techs/Techs';
 import AboutMe from './AboutMe/AboutMe';
