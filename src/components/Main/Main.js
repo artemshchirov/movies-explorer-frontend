a71ec55654dfaa1b1fec710985678d34bb0f3cab
Artem Shchirov <artemshchirov@gmail.com>
2022-09-24 17:09:21 +0300

    add AboutProject, refactor Header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,22 +12,26 @@ const Main = () => {
   return (
     <>
       <header className="header-landing">
-        <Link className="header-landing__logo" to={'/'}>
-          <img src={logo} alt="Логотип 'Movies Explorer'" />
-        </Link>
+        <div className="header-landing__wrapper">
+          <Link className="header-landing__logo" to={'/'}>
+            <img src={logo} alt="Логотип 'Movies Explorer'" />
+          </Link>
 
-        <ul className="header-landing__links">
-          <li>
-            <Link className="header-landing__register link" to="/signup">
-              Регистрация
-            </Link>
-          </li>
-          <li>
-            <Link className="header-landing__login" to="/signin">
-              <button className="header-landing__login-btn link">Войти</button>
-            </Link>
-          </li>
-        </ul>
+          <ul className="header-landing__links">
+            <li>
+              <Link className="header-landing__register link" to="/signup">
+                Регистрация
+              </Link>
+            </li>
+            <li>
+              <Link className="header-landing__login" to="/signin">
+                <button className="header-landing__login-btn link">
+                  Войти
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </header>
       <main className="content page__content">
         <Promo />
