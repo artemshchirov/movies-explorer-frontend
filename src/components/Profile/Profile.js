d4aa4a239e7dee8c87a50fde6d006c102cbb37ac
Artem Shchirov <artemshchirov@gmail.com>
2022-10-02 21:24:42 +0300

    add Profile 320px 768px 1280px

--- src/components/Profile/Profile.js
+++
@@ -1,7 +0,0 @@
-import './Profile.css';
-
-const Profile = () => (
-    <></>
-);
-
-export default Profile;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import './Profile.css';
+import Title from '../Title/Title.jsx';
+import Button from '../Button/Button.jsx';
+
+const Profile = () => (
+  <section className="account">
+    <div className="account__container">
+      <Title style="title_type_main" text="Привет, Артём!" />
+      <div className="account__line-container">
+        <div className="account__line">
+          <p className="account__data account__data_description ">Имя</p>
+          <p className="account__data">Артём</p>
+        </div>
+        <div className="account__divider"></div>
+        <div className="account__line">
+          <p className="account__data account__data_description">E-mail</p>
+          <p className="account__data">pochta@yandex.ru</p>
+        </div>
+      </div>
+      <div className="account__btns">
+        <Button style="account__btn" title="Редактировать" />
+        <Button
+          style="account__btn account__btn_last"
+          title="Выйти из аккаунта"
+        />
+      </div>
+    </div>
+  </section>
+);
+
+export default Profile;
