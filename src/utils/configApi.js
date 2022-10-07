6395ae9d889555c9b9633f3ebebcc3425695f122
Artem Shchirov <artemshchirov@gmail.com>
2022-10-07 17:28:29 +0300

    add api

---
+++ src/utils/configApi.js
@@ -0,0 +1,13 @@
+export const configMainApi = {
+  baseUrl: 'http://localhost:3000',
+  headers: {
+      'Content-Type': 'application/json'
+  }
+}
+
+export const configMoviesApi = {
+  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  headers: {
+      'Content-Type': 'application/json'
+  }
+}
